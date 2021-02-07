3c20cc975be97128fddb7993cf5d2fccdc988966
CaitieReza <caitlinreza@ucsb.edu>
2021-02-07 10:42:24 -0800

    Added biomass estimate radiobuttons widget

--- app.R
+++ app.R
@@ -7,10 +7,17 @@ library(palmerpenguins)
 ui <- fluidPage(
   titlePanel("Visualizing Wild-Caught Stocks"),
   sidebarLayout(
-    sidebarPanel("here are my widgets"),
-    mainPanel("this is where our graphs will go")
+    sidebarPanel("our widgets",
+                 radioButtons(inputId = "penguin_species", 
+                              label = "Biomass Estimates by Species", 
+                              choices = c("Tuna", "Salmon", "Fish 3", "Fish 4", "Fish 5")
+                 )
+    ),
+    mainPanel("here's where our graph will go")
   )
 )
+
+
 
 # Server function
 server <- function(input, output) {}
