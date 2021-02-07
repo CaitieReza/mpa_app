188c1e6f6c5e7ae8b05521e79a4008c21a3c491a
CaitieReza <caitlinreza@ucsb.edu>
2021-02-07 10:52:12 -0800

    Added slide bar and dropdown widgets

--- app.R
+++ app.R
@@ -9,9 +9,18 @@ ui <- fluidPage(
   sidebarLayout(
     sidebarPanel("our widgets",
                  radioButtons(inputId = "penguin_species", 
-                              label = "Biomass Estimates by Species", 
-                              choices = c("Tuna", "Salmon", "Fish 3", "Fish 4", "Fish 5")
-                 )
+                              label = h3("Biomass Estimates by Species"), 
+                              choices = c("Tuna" = 1, "Salmon" = 2, "Fish 3" = 3, "Fish 4" = 4, "Fish 5" = 5)
+                 ),
+                 sliderInput("slider1", 
+                             label = h3("Biomass by Year"), 
+                             min = 1950, 
+                             max = 2020, value = 10),
+                 selectInput("select", label = h3("MPA's by Region"), 
+                             choices = list("Choice 1" = 1, "Choice 2" = 2, "Choice 3" = 3), 
+                             selected = 1)
+                 
+                 
     ),
     mainPanel("here's where our graph will go")
   )
