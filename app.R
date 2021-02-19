4a3e05faa8ead71fc02bf6c74d993cc26f480b08
CaitieReza <caitlinreza@ucsb.edu>
2021-02-19 12:49:26 -0800

    Took from Main

--- app.R
+++ app.R
@@ -1,31 +1,74 @@
 library(shiny)
 library(tidyverse)
 library(palmerpenguins)
+library(shinythemes)
+
+
+#?shinythemes on console shows you all the different themes (if you want to pick a different one)
+
 
 
 # User Interface
-ui <- fluidPage(
-  titlePanel("Visualizing Wild-Caught Stocks"),
-  sidebarLayout(
-    sidebarPanel("our widgets",
-                 radioButtons(inputId = "penguin_species", 
-                              label = h3("Biomass Estimates by Species"), 
-                              choices = c("Tuna" = 1, "Salmon" = 2, "Fish 3" = 3, "Fish 4" = 4, "Fish 5" = 5)
-                 ),
-                 sliderInput("slider1", 
-                             label = h3("Biomass by Year"), 
-                             min = 1950, 
-                             max = 2020, value = 10),
-                 selectInput("select", label = h3("MPA's by Region"), 
-                             choices = list("Choice 1" = 1, "Choice 2" = 2, "Choice 3" = 3), 
-                             selected = 1),
-                 selectInput("select", label = h3("Ecological Factors"), 
-                             choices = list("Mobility" = 1, "Choice 2" = 2, "Choice 3" = 3), 
-                             selected = 1),
-    ),
-    mainPanel("here's where our graph will go")
-  )
+ui <- fluidPage(theme = shinytheme("simplex"),
+                
+                navbarPage("OUR COOL TITLE",
+                           tabPanel("Summary/Information Tab"),
+                           
+                           tabPanel("Tab 1",
+                                    sidebarLayout(
+                                      sidebarPanel("our widget/s!",
+                                                   radioButtons(inputId = "penguin_species", 
+                                                                label = h3("Biomass Estimates by Species"), 
+                                                                choices = c("Tuna" = 1, "Salmon" = 2, "Fish 3" = 3, "Fish 4" = 4, "Fish 5" = 5)
+                                                                
+                                                   )
+                                      ),
+                                      mainPanel("here's where our AWESOME graph will go")
+                                    )
+                           ),
+                           tabPanel("Tab 2",
+                                    sidebarLayout(
+                                      sidebarPanel("our widget/s!",
+                                                   sliderInput("slider1", 
+                                                               label = h3("Biomass by Year"), 
+                                                               min = 1950, 
+                                                               max = 2020, value = 10
+                                                               
+                                                   )
+                                      ),
+                                      mainPanel("here's where our AWESOME graph will go")
+                                    )
+                           ),
+                           tabPanel("Tab 3",
+                                    sidebarLayout(
+                                      sidebarPanel("our widget/s!",
+                                                   selectInput("select", label = h3("MPA's by Region"), 
+                                                               choices = list("Choice 1" = 1, "Choice 2" = 2, "Choice 3" = 3), 
+                                                               selected = 1
+                                                   )
+                                                   
+                                      ),
+                                      mainPanel("here's where our AWESOME graph will go")
+                                    )
+                           ),
+                           tabPanel("Tab 4",
+                                    sidebarLayout(
+                                      sidebarPanel("our widget/s!",
+                                                   selectInput("select", label = h3("Ecological Factors"), 
+                                                               choices = list("Mobility" = 1, "Choice 2" = 2, "Choice 3" = 3), 
+                                                               selected = 1)
+                                      ),
+                                      mainPanel("here's where our AWESOME graph will go")
+                                    )
+                           )
+                           
+                           
+                           
+                )
+                
 )
+
+
 
 
 
