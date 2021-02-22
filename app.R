8c6553958811ddfe0f6d0e36474c5186a30542b0
CaitieReza <caitlinreza@ucsb.edu>
2021-02-22 11:19:12 -0800

    Changed formatting from previous attempt

--- app.R
+++ app.R
@@ -10,64 +10,37 @@ library(shinythemes)
 
 # User Interface
 
-ui <- fluidPage(theme = shinytheme("sandstone"),
-                
-                navbarPage("StockWatch",
-                           tabPanel("Home"),
-                           
-                           tabPanel("Ecology",
-
 ui <- fluidPage(theme = shinytheme("simplex"),
                 
                 navbarPage("OUR COOL TITLE",
                            tabPanel("Summary/Information Tab"),
                            
                            tabPanel("Tab 1",
-
                                     sidebarLayout(
                                       sidebarPanel("our widget/s!",
                                                    radioButtons(inputId = "penguin_species", 
                                                                 label = h3("Biomass Estimates by Species"), 
                                                                 choices = c("Tuna" = 1, "Salmon" = 2, "Fish 3" = 3, "Fish 4" = 4, "Fish 5" = 5)
-
                                                                 
                                                    )
                                       ),
                                       mainPanel("here's where our AWESOME graph will go")
                                     )
                            ),
-                           tabPanel("Biomass",
-
-                                                     
-                                                   )
-                                                   ),
-                                      mainPanel("here's where our AWESOME graph will go")
-                                    )
-                                    ),
                            tabPanel("Tab 2",
-
                                     sidebarLayout(
                                       sidebarPanel("our widget/s!",
                                                    sliderInput("slider1", 
                                                                label = h3("Biomass by Year"), 
                                                                min = 1950, 
                                                                max = 2020, value = 10
-
                                                                
-
-                                                     
-
                                                    )
                                       ),
                                       mainPanel("here's where our AWESOME graph will go")
                                     )
-
                            ),
-                           tabPanel("Fishing Pressure",
-
-                                    ),
                            tabPanel("Tab 3",
-
                                     sidebarLayout(
                                       sidebarPanel("our widget/s!",
                                                    selectInput("select", label = h3("MPA's by Region"), 
@@ -78,13 +51,8 @@ ui <- fluidPage(theme = shinytheme("simplex"),
                                       ),
                                       mainPanel("here's where our AWESOME graph will go")
                                     )
-
                            ),
-                           tabPanel("Exploring Trends",
-
-                                    ),
                            tabPanel("Tab 4",
-
                                     sidebarLayout(
                                       sidebarPanel("our widget/s!",
                                                    selectInput("select", label = h3("Ecological Factors"), 
@@ -93,24 +61,13 @@ ui <- fluidPage(theme = shinytheme("simplex"),
                                       ),
                                       mainPanel("here's where our AWESOME graph will go")
                                     )
-
                            )
                            
                            
                            
                 )
                 
-
-                                    )
-                           
-                           
-                           
-                           )
-  
-
 )
-  
-  
 
 
 
